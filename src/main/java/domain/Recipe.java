5033b6cc24c74367741d5aa93121cfe7f216b792
bperquku <bperquku@gmail.com>
2018-10-25 13:28:03 +0200

    relating Recipe and Ingredient entities

--- src/main/java/domain/Recipe.java
+++ src/main/java/domain/Recipe.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -23,9 +26,13 @@ public class Recipe {
 	private String directions;
 	// TODO: add difficulty
 	// Difficulty difficulty;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
+	private Set<Ingredient> ingredients;
+
 	@Lob
 	private Byte[] image;
-
+	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 
@@ -107,6 +114,14 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
