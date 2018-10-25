1cc00a678690fbc16704358ae9ab8d9b6787b93e
bperquku <bperquku@gmail.com>
2018-10-25 13:42:44 +0200

    one 2 one assignment

--- src/main/java/domain/Ingredient.java
+++ src/main/java/domain/Ingredient.java
@@ -1,25 +1,29 @@
 package domain;
 
 import java.math.BigDecimal;
-
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String description;
 	private BigDecimal amount;
-	
+
 	@ManyToOne
 	private Recipe recipe;
+
+	@OneToOne(fetch=FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
 
 	public Long getId() {
 		return id;
@@ -52,6 +56,13 @@ public class Ingredient {
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
 	}
-	
-	
+
+	public UnitOfMeasure getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
+	}
+
 }
