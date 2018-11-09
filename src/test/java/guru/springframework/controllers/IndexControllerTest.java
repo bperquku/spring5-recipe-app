9bd54d1c5cf41a2f514a21f685e7d5414b0b96de
bperquku <bperquku@gmail.com>
2018-11-09 14:11:41 +0100

    Intro to mockmvc

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,20 +1,26 @@
 package guru.springframework.controllers;
 
 import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Test;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
-import guru.springframework.controllers.IndexController;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 
@@ -33,6 +39,12 @@ public class IndexControllerTest {
 		MockitoAnnotations.initMocks(this);
 		controller = new IndexController(recipeService);
 	}
+	
+	@Test
+	public void testMockMVC() throws Exception{
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+	}
 
 	@Test
 	public void testGetIndexPage() {
@@ -44,7 +56,7 @@ public class IndexControllerTest {
 		recipes.add(recipe1);
 		
 		Recipe recipe2 = new Recipe();
-		recipe2.setId(1L);
+		recipe2.setId(2L);
 		
 		recipes.add(recipe2);
 		
