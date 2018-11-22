e73df2fef8206cb491d0ebdbd73a378ed8a5baa7
bperquku <bperquku@gmail.com>
2018-11-22 16:03:06 +0100

    displaying enum in option dropdown list

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -28,8 +28,7 @@ public class IngredientController {
     this.unitOfMeasureService = unitOfMeasureService;
   }
 
-  @GetMapping
-  @RequestMapping("/recipe/{recipeId}/ingredients")
+  @GetMapping("/recipe/{recipeId}/ingredients")
   public String listIngredients(@PathVariable String recipeId, Model model) {
     log.debug("Getting ingredient list for recipe id: " + recipeId);
 
@@ -39,8 +38,7 @@ public class IngredientController {
     return "recipe/ingredient/list";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+  @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
   public String showRecipeIngredient(
       @PathVariable String recipeId, @PathVariable String id, Model model) {
     model.addAttribute(
@@ -49,8 +47,7 @@ public class IngredientController {
     return "recipe/ingredient/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/{id}/update")
+  @GetMapping("recipe/{recipeId}/ingredient/{id}/update")
   public String updateRecipeIngredient(
       @PathVariable String recipeId, @PathVariable String id, Model model) {
     model.addAttribute(
@@ -75,8 +72,7 @@ public class IngredientController {
         + "/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/new")
+  @GetMapping("recipe/{recipeId}/ingredient/new")
   public String newRecipe(@PathVariable String recipeId, Model model) {
     RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
 
