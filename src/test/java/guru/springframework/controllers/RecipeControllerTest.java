3043bdde5c28173e775ada4ebcf620668e5a1b69
bperquku <bperquku@gmail.com>
2018-11-28 02:03:34 +0100

    Adding Exception and annotating as ResponseStatus

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -21,81 +21,96 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
 
 public class RecipeControllerTest {
 
-	@Mock
-	RecipeService recipeService;
+  @Mock RecipeService recipeService;
 
-	RecipeController controller;
+  RecipeController controller;
 
-	MockMvc mockMvc;
+  MockMvc mockMvc;
 
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		controller = new RecipeController(recipeService);
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-	}
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    controller = new RecipeController(recipeService);
+    mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+  }
 
-	@Test
-	public void testGetRecipe() throws Exception {
-		Recipe recipe = new Recipe();
-		recipe.setId(1L);
+  @Test
+  public void testGetRecipe() throws Exception {
+    Recipe recipe = new Recipe();
+    recipe.setId(1L);
 
-		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-		when(recipeService.findById(anyLong())).thenReturn(recipe);
+    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    when(recipeService.findById(anyLong())).thenReturn(recipe);
 
-		mockMvc.perform(get("/recipe/1/show"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("recipe/show"))
-		.andExpect(model().attributeExists("recipe"));
-	}
+    mockMvc
+        .perform(get("/recipe/1/show"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("recipe/show"))
+        .andExpect(model().attributeExists("recipe"));
+  }
 
-	@Test
-	public void testGetNewRecipeForm() throws Exception {
-		mockMvc.perform(get("/recipe/new"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("recipe/recipeform"))
-		.andExpect(model().attributeExists("recipe"));
-	}
-	
-	@Test
-	public void testPostNewRecipeForm() throws Exception {
-		RecipeCommand command = new RecipeCommand();
-		command.setId(2L);
-		
-		when(recipeService.saveRecipeCommand(any())).thenReturn(command);
-		
-		mockMvc.perform(post("/recipe")
-		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-		.param("id", "")
-		.param("description", "some string"))
-		
-		.andExpect(status().is3xxRedirection())
-		.andExpect(view().name("redirect:/recipe/2/show"));
-	}
-	
-	@Test
-	public void testGetUpdateView() throws Exception {
-		RecipeCommand command = new RecipeCommand();
-		command.setId(2L);
-		
-		when(recipeService.findCommandById(anyLong())).thenReturn(command);
-		
-		mockMvc.perform(get("/recipe/1/update"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("recipe/recipeform"))
-		.andExpect(model().attributeExists("recipe"));
-	}
-	
-	@Test
-	public void testDeleteAction() throws Exception{
-		mockMvc.perform(get("/recipe/1/delete"))
-		.andExpect(status().is3xxRedirection())
-		.andExpect(view().name("redirect:/"));
-		
-		verify(recipeService,times(1)).deleteById(anyLong());
-	}
+  @Test
+  public void testGetRecipeNotFound() throws Exception {
+    Recipe recipe = new Recipe();
+    recipe.setId(1L);
+
+    when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+    mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void testGetNewRecipeForm() throws Exception {
+    mockMvc
+        .perform(get("/recipe/new"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("recipe/recipeform"))
+        .andExpect(model().attributeExists("recipe"));
+  }
+
+  @Test
+  public void testPostNewRecipeForm() throws Exception {
+    RecipeCommand command = new RecipeCommand();
+    command.setId(2L);
+
+    when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+    mockMvc
+        .perform(
+            post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/recipe/2/show"));
+  }
+
+  @Test
+  public void testGetUpdateView() throws Exception {
+    RecipeCommand command = new RecipeCommand();
+    command.setId(2L);
+
+    when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+    mockMvc
+        .perform(get("/recipe/1/update"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("recipe/recipeform"))
+        .andExpect(model().attributeExists("recipe"));
+  }
+
+  @Test
+  public void testDeleteAction() throws Exception {
+    mockMvc
+        .perform(get("/recipe/1/delete"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/"));
+
+    verify(recipeService, times(1)).deleteById(anyLong());
+  }
 }
--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -19,69 +19,75 @@ import org.mockito.MockitoAnnotations;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.services.RecipeServiceImpl;
 
 public class RecipeServiceImplTest {
 
-	RecipeServiceImpl recipeService;
+  RecipeServiceImpl recipeService;
 
-	@Mock
-	RecipeRepository recipeRepository;
+  @Mock RecipeRepository recipeRepository;
 
-	@Mock
-	RecipeToRecipeCommand recipeToRecipeCommand;
+  @Mock RecipeToRecipeCommand recipeToRecipeCommand;
 
-	@Mock
-	RecipeCommandToRecipe recipeCommandToRecipe;
+  @Mock RecipeCommandToRecipe recipeCommandToRecipe;
 
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
 
-		recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
-	}
+    recipeService =
+        new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
+  }
 
-	@Test
-	public void getRecipeByIdTest() throws Exception {
-		Recipe recipe = new Recipe();
-		recipe.setId(1L);
-		Optional<Recipe> recipeOptional = Optional.of(recipe);
+  @Test
+  public void getRecipeByIdTest() throws Exception {
+    Recipe recipe = new Recipe();
+    recipe.setId(1L);
+    Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-		when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+    when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
 
-		Recipe recipeReturned = recipeService.findById(1L);
+    Recipe recipeReturned = recipeService.findById(1L);
 
-		assertNotNull("Null recipe returned", recipeReturned);
-		verify(recipeRepository, times(1)).findById(anyLong());
-		verify(recipeRepository, never()).findAll();
-	}
+    assertNotNull("Null recipe returned", recipeReturned);
+    verify(recipeRepository, times(1)).findById(anyLong());
+    verify(recipeRepository, never()).findAll();
+  }
 
-	@Test
-	public void getRecipesTest() throws Exception {
-		Recipe recipe = new Recipe();
-		HashSet<Recipe> recipesData = new HashSet<>();
-		recipesData.add(recipe);
+  @Test(expected = NotFoundException.class)
+  public void getRecipeByIdTestNotFound() {
+    Optional<Recipe> recipeOptional = Optional.empty();
+    when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
 
-		when(recipeService.getRecipes()).thenReturn(recipesData);
+    Recipe recipeReturned = recipeService.findById(1l);
+  }
 
-		Set<Recipe> recipes = recipeService.getRecipes();
-		assertEquals(1, recipes.size());
+  @Test
+  public void getRecipesTest() throws Exception {
+    Recipe recipe = new Recipe();
+    HashSet<Recipe> recipesData = new HashSet<>();
+    recipesData.add(recipe);
 
-		verify(recipeRepository, times(1)).findAll();
-		verify(recipeRepository, never()).findById(anyLong());
+    when(recipeService.getRecipes()).thenReturn(recipesData);
 
-	}
+    Set<Recipe> recipes = recipeService.getRecipes();
+    assertEquals(1, recipes.size());
 
-	@Test
-	public void testDeleteById() throws Exception{
-		//given
-		Long idToDelete = Long.valueOf(2L);
-		recipeService.deleteById(idToDelete);
+    verify(recipeRepository, times(1)).findAll();
+    verify(recipeRepository, never()).findById(anyLong());
+  }
 
-		// no 'when', since method has void return type
-		
-		//then
-		verify(recipeRepository,times(1)).deleteById(anyLong());
-	}
+  @Test
+  public void testDeleteById() throws Exception {
+    // given
+    Long idToDelete = Long.valueOf(2L);
+    recipeService.deleteById(idToDelete);
+
+    // no 'when', since method has void return type
+
+    // then
+    verify(recipeRepository, times(1)).deleteById(anyLong());
+  }
 }
