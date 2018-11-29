617481bd4514c3fab596c2e059bc848e02a7dd73
bperquku <bperquku@gmail.com>
2018-11-29 09:20:59 +0100

    Adding exception handler

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,14 +1,18 @@
 package guru.springframework.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,43 +20,52 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RecipeController {
 
-	private final RecipeService recipeService;
+  private final RecipeService recipeService;
 
-	public RecipeController(RecipeService recipeService) {
-		this.recipeService = recipeService;
-	}
+  public RecipeController(RecipeService recipeService) {
+    this.recipeService = recipeService;
+  }
 
-	@GetMapping("/recipe/{id}/show")
-	public String showById(@PathVariable String id, Model model) {
-		model.addAttribute("recipe", recipeService.findById(new Long(id)));
+  @GetMapping("/recipe/{id}/show")
+  public String showById(@PathVariable String id, Model model) {
+    model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
-		return "recipe/show";
-	}
+    return "recipe/show";
+  }
 
-	@GetMapping("recipe/new")
-	public String newRecipe(Model model) {
-		model.addAttribute("recipe", new RecipeCommand());
+  @GetMapping("recipe/new")
+  public String newRecipe(Model model) {
+    model.addAttribute("recipe", new RecipeCommand());
 
-		return "recipe/recipeform";
-	}
+    return "recipe/recipeform";
+  }
 
-	@GetMapping("recipe/{id}/update")
-	public String updateRecipe(@PathVariable String id, Model model) {
-		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-		return "recipe/recipeform";
-	}
+  @GetMapping("recipe/{id}/update")
+  public String updateRecipe(@PathVariable String id, Model model) {
+    model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+    return "recipe/recipeform";
+  }
 
-	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
-		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+  @PostMapping("recipe")
+  public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-		return "redirect:/recipe/" + savedCommand.getId() + "/show";
-	}
+    return "redirect:/recipe/" + savedCommand.getId() + "/show";
+  }
 
-	@GetMapping("recipe/{id}/delete")
-	public String deleteById(@PathVariable String id) {
-		log.debug("Deleting id: " + id);
-		recipeService.deleteById(Long.valueOf(id));
-		return "redirect:/";
-	}
+  @GetMapping("recipe/{id}/delete")
+  public String deleteById(@PathVariable String id) {
+    log.debug("Deleting id: " + id);
+    recipeService.deleteById(Long.valueOf(id));
+    return "redirect:/";
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NotFoundException.class)
+  public ModelAndView handeNotFound() {
+    log.error("Handling not found exception");
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("404error");
+    return modelAndView;
+  }
 }
