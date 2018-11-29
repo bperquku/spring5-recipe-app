f2e14f02d69706829911669763ca79c6840ae57b
bperquku <bperquku@gmail.com>
2018-11-29 10:30:06 +0100

    adding number format exception handler

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import guru.springframework.commands.RecipeCommand;
@@ -29,7 +28,6 @@ public class RecipeController {
   @GetMapping("/recipe/{id}/show")
   public String showById(@PathVariable String id, Model model) {
     model.addAttribute("recipe", recipeService.findById(new Long(id)));
-
     return "recipe/show";
   }
 
@@ -62,12 +60,31 @@ public class RecipeController {
 
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ExceptionHandler(NotFoundException.class)
-  public ModelAndView handeNotFound(Exception exception) {
+  public ModelAndView handleNotFound(Exception exception) {
+
     log.error("Handling not found exception");
     log.error(exception.getMessage());
+
     ModelAndView modelAndView = new ModelAndView();
+
     modelAndView.setViewName("404error");
     modelAndView.addObject("exception", exception);
+
+    return modelAndView;
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(NumberFormatException.class)
+  public ModelAndView handleNumberFormat(Exception exception) {
+
+    log.error("Handling Number Format Exception");
+    log.error(exception.getMessage());
+
+    ModelAndView modelAndView = new ModelAndView();
+
+    modelAndView.setViewName("400error");
+    modelAndView.addObject("exception", exception);
+
     return modelAndView;
   }
 }
